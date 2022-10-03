be48d830263cf3496a8c1dab868932fc295a3ad9
renniequin@gmail.com <renniequin@gmail.com>
2022-10-03 21:58:12 +0300

    links the api using users input and displays the screenshot taken of the page

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ submitBtn.addEventListener('click', ()=> {
     getScreenShot(link)
 })
 
+async function getScreenShot(link) { 
+    try {
+        await axios 
+        .get (`https://api.browshot.com/api/v1/screenshot/create?url=${link.value}/&instance_id=12&size=page&key=CXirmDIyiJlRUSK9b7Ylhdkon41r`)
+
+        .then(response => {
+            console.log(response.data);
+            image.src = response.data.screenshot_url ;
+            console.log("Cost is "+ response.data.cost);
+        });
+    }catch (error){
+        console.log('error');
+    }
+
+}
