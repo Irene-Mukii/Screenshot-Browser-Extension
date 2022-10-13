192b6b592079a7c0fc52c4543d1ab5d3d75a4edd
renniequin@gmail.com <renniequin@gmail.com>
2022-10-13 11:34:40 +0300

    Uses chrome tabs API to retrieve curent page then passes the link to screenshot API ---- not sure why, how and when tabs is passed into the .query function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //input variables
-const link = tab.url; //to get current url ----gets browser extension page... not correct
-const instanceId = document.querySelector('#instance_id');
+const inputedlink = document.querySelector('#url');//tab.url; //to get current url ----gets browser extension @FIXME page... not correct
+//const instanceId = document.querySelector('#instance_id');
 const submitBtn = document.querySelector('.screenshot-btn')
 
 //output variables
@@ -10,23 +10,41 @@ const imageLink = document.querySelector('.screenshot-image-link')
 
 //getting screenshot
 submitBtn.addEventListener('click', ()=> {
-    imageLink.innerHTML = link ;
-    //getScreenShot(link)
+    // imageLink.innerHTML = link ;
+    let link = getCurrentTab();
+    getScreenShot(link)
 })
 
-// async function getScreenShot(link) { 
-//     try {
-//         await axios 
-//         .get (`https://api.browshot.com/api/v1/screenshot/create?url=${link}/&instance_id=12&size=page&key=CXirmDIyiJlRUSK9b7Ylhdkon41r`)
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  await chrome.tabs.query(queryOptions, function(tabs){
+    var tab = tabs[0];
+    console.log(tab);
+    var activeUrl = tab.url;
+    console.log(activeUrl)
+  });
+  
+  return activeUrl;
+}
 
-//         .then(response => {
-//             console.log(response.data);
-//             imageLink.innerHTML = link ;
-//             image.src = response.data.screenshot_url;
-//             console.log("Cost is "+ response.data.cost);
-//         });
-//     }catch (error){
-//         console.log('error');
-//     }
 
-// }
+async function getScreenShot(link) { 
+    try {
+        await axios 
+        .get (`https://api.browshot.com/api/v1/screenshot/create?url=${link.value}/&instance_id=12&size=page&key=CXirmDIyiJlRUSK9b7Ylhdkon41r`)
+
+        .then(response => {
+            console.log(response.data);
+            imageLink.innerHTML = link.value ; 
+            image.src = response.data.screenshot_url;
+            console.log("Cost is "+ response.data.cost);
+
+            //@TODO store screenshots or their url in localstorage maybe use setItem?
+
+        });
+    }catch (error){
+        console.log('error');
+    }
+
+}
